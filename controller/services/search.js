27a3c216244cb5e051a41c74d6ba9c03ec47c261
remindev <reminremin01@gmail.com>
2022-12-03 13:37:06 +0530

    Search update

--- controller/services/search.js
+++ controller/services/search.js
@@ -4,36 +4,45 @@ export const search = async (query, pages) => {
     try {
 
         query = query?.trim();
-        if(query=='') query = ' ';
 
         // let outputData = await db.products.find(
         //     { $text: { $search: query } },
         //     { score: { $meta: "textScore" } }
         // ).sort({ score: { $meta: "textScore" } });
 
-        let outputData = await db.products.aggregate([
-            {
-                $search: {
-                    index: 'products_search',
-                    text: {
-                        query: query,
-                        path: {
-                            'wildcard': '*'
+        let outputData;
+
+        if (query) {
+
+            outputData = await db.products.aggregate([
+                {
+                    $search: {
+                        index: 'products_search',
+                        text: {
+                            query: query,
+                            path: {
+                                'wildcard': '*'
+                            }
                         }
                     }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        views: 0,
+                        impressions: 0, addedToCart: 0,
+                        interactions: 0,
+                        reachedCheckout: 0,
+                        purchased: 0
+                    }
                 }
-            },
-            {
-                $project:{
-                    _id:0,
-                    views:0,
-                    impressions:0,addedToCart:0,
-                    interactions:0,
-                    reachedCheckout:0,
-                    purchased:0
-                }
-            }
-        ])
+            ]);
+
+        } else {
+
+            outputData = await db.products.find({});
+
+        };
 
         // returning result
         return outputData;
